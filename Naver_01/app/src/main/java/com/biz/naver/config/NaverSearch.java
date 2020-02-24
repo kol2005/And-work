13f0268e83674bac57294e294e6749acfd01454e
kol2005 <kol2005@naver.com>
2020-02-24 09:52:03 +0900

    naver

---
+++ Naver_01/app/src/main/java/com/biz/naver/config/NaverSearch.java
@@ -0,0 +1,6 @@
+package com.biz.naver.config;
+
+public class NaverSearch {
+
+    private final String naver_movie_url = "https://openapi.naver.com/v1/search/movie.json";
+}
---
+++ Naver_01/app/src/main/java/com/biz/naver/config/NaverSecurSample.java
@@ -0,0 +1,8 @@
+package com.biz.naver.config;
+
+public class NaverSecurSample {
+
+    public final static String NAVER_ID = "YOUR NAVER ID";
+    public final static String NAVER_SEC = "YOUR NAVER SEC";
+
+}
